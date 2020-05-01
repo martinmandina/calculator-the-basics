ed537d4a405afac249a3aaf3730a78a171a60063
martinsmandina <martinsmandina@gmail.com>
2020-05-01 10:23:46 +0300

    create js script and added variable for add and subtract

---
+++ js/scripts.js
@@ -0,0 +1,15 @@
+var add = function(number1, number2) {
+  return number1 + number2;
+};
+var number1 = parseInt(prompt("Enter a number to add:"));
+var number2 = parseInt(prompt("Enter another number to add:"));
+var result = (add(number1, number2));
+alert(result);
+
+var subtract = function(number1, number2) {
+  return number1 - number2;
+};
+var number1 = parseInt(prompt("Enter number to subtract:"));
+var number2 = parseInt(prompt("Enter another number to subtract:"));
+var result = (subtract(number1, number2));
+alert(result);
